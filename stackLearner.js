e5ddde91dbd7257919e1f35dce85ad770c6c04ef
EmadAA <emaduddin167@gmail.com>
2023-04-07 02:35:56 +0600

    emad

--- stackLearner.js
+++ stackLearner.js
@@ -6,3 +6,53 @@
 // null
 // false
 // this 6 types are considered as a falsy value
+
+// Hexadecimal number 
+// let hex = 0xae;
+// console.log(hex);
+
+// Octal Number 
+// let oct = 0100;
+// console.log(oct);
+
+//Operator 
+// + - * / % ++ -- 
+
+// Logical Operator
+// && || !
+
+//Bitwise Operator
+// & | ~ ^ <<  >>
+
+
+// typeof operator
+
+// console.log(typeof '10');
+
+// Builtin Math function/object
+
+// Math.E
+// Math.PI
+// Math.floor()
+// Math.ceil()
+// Math.abs()
+// Math.max(100, 520)
+// Math.min(100, 542)
+// Math.random()
+// Math.pow(5 , 2)
+// Math.sqrt(100)
+
+// Date Object
+
+// let date = new Date();
+// console.log(date);
+// console.log(date.toDateString())
+// date.toTimeString()
+// date.toLocaleString()
+// date.getFullYear()
+// date.getMonth()
+// date.getHours()
+// date.getMinutes()
+// date.getSeconds()
+// date.getMilliseconds()
+
